9d28f8fc7ab499ce12229e12fa6cdf1cf0497919
Sahra Ghalebikesabi <s.ghalebikesabi@gmail.com>
2020-11-27 14:06:22 +0000

    bla

--- src/utils.py
+++ src/utils.py
@@ -22,13 +22,22 @@ def impute(model, data, args, kwargs):
         recon, variational_params, latent_samples = model(data_filled, M_miss, test_mode=True, L=args.num_samples)
         variational_params['py'] = torch.ones_like(variational_params['qy']).to(args.device)/variational_params['qy'].shape[1]
 
-        log_p_xmis_given_z_r =  log_normal(latent_samples['xmis']*M_miss, recon['xmis']*M_miss, torch.tensor([0.5]).to(args.device)) 
-        # log_p_xmis_given_z_r = 0 # log_normal(latent_samples['xmis']*M_miss, recon['xmis']*M_miss, torch.tensor([0.5])) 
-        log_p_xmis_given_z_r += log_normal(latent_samples['xmis']*M_obs, recon['xmis']*M_obs, torch.tensor([0.5]).to(args.device)) * args.pi # ! 0.25
-        log_p_xmis_given_z_r += log_normal(data_filled*M_obs, recon['xmis']*M_obs, torch.tensor([0.5]).to(args.device)) * (1 - args.pi) # ! 0.25
+        if 'PSMVAE' in args.model_class:
+          log_p_xmis_given_z_r =  log_normal(latent_samples['xmis']*M_miss, recon['xmis']*M_miss, torch.tensor([0.5]).to(args.device)) 
+          # log_p_xmis_given_z_r = 0 # log_normal(latent_samples['xmis']*M_miss, recon['xmis']*M_miss, torch.tensor([0.5])) 
+          log_p_xmis_given_z_r += log_normal(latent_samples['xmis']*M_obs, recon['xmis']*M_obs, torch.tensor([0.5]).to(args.device)) * args.pi # ! 0.25
+          log_p_xmis_given_z_r += log_normal(data_filled*M_obs, recon['xmis']*M_obs, torch.tensor([0.5]).to(args.device)) * (1 - args.pi) # ! 0.25
+        else:
+          log_p_xmis_given_z_r = 0
+
         log_p_xobs_given_z_r =  M_obs*log_normal(data_filled, recon['xobs'], torch.tensor([0.5]).to(args.device)) # ! 0.25
         
         log_p_z_given_r = log_normal(latent_samples['z'], variational_params['z_mu_prior'], torch.exp(variational_params['z_logvar_prior']))
+
+        # variational_params['z_mu_prior'].shape
+        # torch.Size([10, 1, 20])
+        # latent_samples['z'].shape
+        # torch.Size([10, 20, 1, 20])
 
         log_p_r = variational_params['py']
 
